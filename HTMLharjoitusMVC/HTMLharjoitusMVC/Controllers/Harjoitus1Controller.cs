51247d86257db8ed77c9a33c3536d01f5ae3d9df
Timo Tirkkonen <timo.tirkkonen@student.careeria.fi>
2019-05-22 09:49:35 +0300

    Tallennus 22.5.2019

---
+++ HTMLharjoitusMVC/HTMLharjoitusMVC/Controllers/Harjoitus1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLharjoitusMVC.Controllers
+{
+    public class Harjoitus1Controller : Controller
+    {
+        // GET: Harjoitus1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HTMLharjoitusMVC/HTMLharjoitusMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HTMLharjoitusMVC.MvcApplication" Language="C#" %>
